e37b27c4905c055a90f0429f969c5b4bfadcbc44
uneclipse <100725444+uneclipse@users.noreply.github.com>
2024-08-19 12:35:24 +0200

    Update espresso_shot_simulation.py

--- espresso_shot_simulation.py
+++ espresso_shot_simulation.py
@@ -5,7 +5,7 @@ from scipy.integrate import solve_ivp
 # Parameters
 R = 0.014  # Radius of the inner water sphere (m)
 d = 0.001  # Thickness of the steel shell (m)
-outer_coffee_thickness = 0.0079  # Thickness of the outer water layer (m)   originally 0.0079 / 0.0059
+outer_coffee_thickness = 0.0079  # Thickness of the coffee layer (m)
 air_thickness = 0.05  # Thickness of the outer air layer (m)
 k_w = 0.6  # Thermal conductivity of water (W/m·K)
 rho_w = 1000  # Density of water (kg/m³)                    glycerine vs water
@@ -19,8 +19,6 @@ c_air = 1005  # Specific heat capacity of air (J/kg·K)
 T_initial = 253 # or 203 for HC # Initial temperature of the sphere (K)                           
 T_outer_coffee_initial = 338  # Initial temperature of the outer coffee layer (K)
 T_air = 293  # Fixed temperature of the air outside the outer coffee layer (K)
-time_end = 2  # Total simulation time (s)
-N = 400  # Number of radial points
 
 # Time discretization
 dt = 1  # Time step (s)
@@ -29,20 +27,19 @@ time_end = 13
 time_points = [0, 0.5, 1, 2, 5, 13]
 
 # Discretization
-r_w = np.linspace(0, R, 1400)  # Radial points in the inner water region
-r_s = np.linspace(R, R + d, 100)  # Radial points in the steel region
-r_aw = np.linspace(R + d, R + d + outer_coffee_thickness, 790)  # Radial points in the outer water layer
+r_w = np.linspace(0, R, 1400)                                                                              # Radial points in the inner water region
+r_s = np.linspace(R, R + d, 100)                                                                           # Radial points in the steel region
+r_c = np.linspace(R + d, R + d + outer_coffee_thickness, 790)                                             # Radial points in the outer coffee layer
 r_air = np.linspace(R + d + outer_coffee_thickness, R + d + outer_coffee_thickness + air_thickness, 5000)  # Radial points in the air layer
-
-# Combine all radial points and set up initial temperature distribution
-r = np.concatenate([r_w, r_s, r_aw, r_air])
+r = np.linspace(0, R + d + air_thickness, 1400 + 100 + 790 + 5000)  # Radial points in the air layer
+dr = r[1] - r[0]
 
 # Load temperatures
 with open('n70_temperatures_apres_etape_3.npy', 'rb') as f:
     last_temp_profile = np.load(f)
 
 initial_temp = np.concatenate([last_temp_profile[:1500],
-                                 np.full_like(r_aw, T_outer_coffee_initial), 
+                                 np.full_like(r_c, T_outer_coffee_initial), 
                                  np.full_like(r_air, T_air)])
 
 # Thermal diffusivities
@@ -51,8 +48,6 @@ alpha_s = k_s / (rho_s * c_s)
 alpha_aw = k_w / (rho_w * c_w)
 alpha_air = k_air / (rho_air * c_air)
 
-r = np.linspace(0, R + d + air_thickness, 1400 + 100 + 790 + 5000)  # Radial points in the air layer
-dr = r[1] - r[0]
 
 # Function to compute the heat equation
 def heat_equation(t, T):
@@ -91,6 +86,7 @@ initial_conditions = initial_temp
 # Solve the ODE
 solution = solve_ivp(heat_equation, [0, time_end], initial_conditions, t_eval=time_points, method='BDF')
 
+# Save the temperature profile for later use
 temps = solution.y[:2290,-1]
 np.save('n70_temperatures_apres_etape_4', temps)
 
@@ -101,8 +97,7 @@ print(coffee_temp-273)
 plt.figure(figsize=(12, 8))
 
 for i in range(0, len(time_points), max(1, len(time_points) // 10)):
-    plt.plot(np.concatenate((r_w, r_s, r_aw, r_air))*1000, solution.y[:, i]-273, label=f't={time_points[i]:.1f}s')
-
+    plt.plot(np.concatenate((r_w, r_s, r_c, r_air))*1000, solution.y[:, i]-273, label=f't={time_points[i]:.1f}s')
 
 plt.vlines(14, -70, 90, linestyle='dashed')
 plt.vlines(15, -70, 90, linestyle='dashed')
